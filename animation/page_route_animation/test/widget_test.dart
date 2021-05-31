0f9ab1d7b06cc8356aa3479fbbfd56d4e505a8fc
Raul Li <raul.guoping@gmail.com>
2021-05-31 06:10:02 -0700

    animation/page_route_animation

--- animation/page_route_animation/test/widget_test.dart
+++ animation/page_route_animation/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:page_route_animation/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Page1());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
