0970a69ad7fc9b93688c6add95af30023b418673
Raul Li <raul.guoping@gmail.com>
2021-06-01 01:39:40 -0700

    effects recipes

---
+++ effects/gradient_bubbles/android/app/src/main/kotlin/com/example/gradient_bubbles/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gradient_bubbles
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ effects/parallax_scrolling/android/app/src/main/kotlin/com/example/parallax_scrolling/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parallax_scrolling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
