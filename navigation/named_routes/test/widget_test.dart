3e25dfd7ea5e4bc3926f44855a500b46905866dd
Raul Li <raul.guoping@gmail.com>
2021-06-01 03:57:52 -0700

    navigation/named_routes

--- navigation/named_routes/test/widget_test.dart
+++ navigation/named_routes/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:named_routes/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(FirstScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
